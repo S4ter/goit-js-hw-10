560bed8c5f9de456a7df433e9fb9f760d507fa5a
S4ter <119447102+S4ter@users.noreply.github.com>
2023-05-13 18:11:23 +0000

    Deploying to gh-pages from @ S4ter/goit-js-hw-10@75a3c08913fcc8fecfb5b8f5b2bf81bef3d68136 🚀

---
+++ index.d28d2e96.js
@@ -0,0 +1,2 @@
+!function(){var n;console.log((n="poland",new Promise((function(o,t){fetch("https://restcountries.com/v3.1/name/".concat(n),{method:"GET"}).then((function(n){if(n.ok)return n.json();t("Error ".concat(n.status))})).then((function(n){o(n)}))}))))}();
+//# sourceMappingURL=index.d28d2e96.js.map
