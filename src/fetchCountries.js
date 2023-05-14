4eb74dc0c6429d26f29a65993755ec6295d0052c
Patryk Stefański <119447102+S4ter@users.noreply.github.com>
2023-05-14 23:58:26 +0200

    e

--- src/fetchCountries.js
+++ src/fetchCountries.js
@@ -2,9 +2,12 @@ export { fetchCountries };
 
 function fetchCountries(name) {
   return new Promise((resolve, reject) => {
-    fetch(`https://restcountries.com/v3.1/name/${name}`, {
-      method: 'GET',
-    })
+    fetch(
+      `https://restcountries.com/v3.1/name/${name}?fields=name,capital,population,flags,languages`,
+      {
+        method: 'GET',
+      }
+    )
       .then(response => {
         if (!response.ok) {
           reject(`Error ${response.status}`);
--- src/index.js
+++ src/index.js
@@ -2,3 +2,13 @@ import './css/styles.css';
 import { fetchCountries } from './fetchCountries.js';
 const DEBOUNCE_DELAY = 300;
 console.log(fetchCountries('poland'));
+const inputBox = document.querySelector('#search-box');
+const countryList = document.querySelector('.country-list');
+const countryInfo = document.querySelector('.country-info');
+let test = [];
+let inputValue = () => {
+  test.push(inputBox.value);
+  console.log(test);
+};
+inputBox.addEventListener('input', inputValue);
+console.log(inputValue);
